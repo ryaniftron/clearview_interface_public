7468421fb8434d3e47e14d6d8f6540ffefde1407
ryaniftron <ryan.iftron@gmail.com>
2020-02-06 00:54:16 -0700

    Add more demos including startup of connectivity and version check. Add demo for all reports

--- src/main.py
+++ src/main.py
@@ -1,25 +1,39 @@
 import ClearView
 import time
+import logging
+
+# Create a custom logger
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__": #example usage
     rx_id = 1   #ensure the cv is set to that rx_id
     bc_id = 0
     cv = ClearView.ClearView(
         port= '/dev/ttyUSB0', #port name, COMX on windows
-        debug = True,
+        debug = False,
         robust = True, #checks all data sent was actually received. Slows stuff down. TODO not implemented well yet 
         timeout = 0.1, #serial read timeout
         )
+
+
    
+    cv.set_receiver_address(bc_id,1)
+    time.sleep(1)
+
     ### Change receiver address from 4 to 1
     #cv.set_receiver_address(4,1)
 
     ### Set all receivers to address 1
     #cv.set_receiver_address(0,1)
 
-    ### Get a list of connected receivers
+    ### Get a list of connected receivers and software versions
     connected_receivers = cv.get_connected_receiver_list()
     print(connected_receivers)
+    for rxaddr,conn_stat in connected_receivers.items():
+        if conn_stat == True:
+            print("Receiver #",rxaddr," is connected and running software version ", cv.get_model_version(rxaddr).software_version_major)
+
+    
 
     
     ### Antenna Mode
@@ -49,7 +63,7 @@ if __name__ == "__main__": #example usage
 
 
     #### Broadcast OSD Message
-    #cv.set_all_osd_message("HELLO ALL")
+    #cv.set_all_osd_meget_band_channelssage("HELLO ALL")
 
 
     ###### Video Mode (show live,spectrum analyzer, menu)
@@ -63,7 +77,7 @@ if __name__ == "__main__": #example usage
     # cv.reset_lock(rx_id)
 
     ##### Video Format
-    # formats = ["n","p","a"]
+    # formats = ["n","p","a","n","p","a"]
     # for format_i in formats:
     #     print("vf:",format_i)
     #     cv.set_video_format(rx_id,format_i)
@@ -84,6 +98,44 @@ if __name__ == "__main__": #example usage
     #     print("Setting osd to position",i)
     #     cv.set_osd_position(rx_id,i)
     #     time.sleep(5)
+
+
+    ### Run the report debugger
+    #cv.get_report_cstm(rx_id)
+    # for i in range(10):
+    #     print("Setting id to ",i)
+    #     cv.set_receiver_address(bc_id,i)
+    #     time.sleep(1)
+    #     print(cv.get_address(bc_id))
+    #     print("\n\n")
+    #     time.sleep(1)
+
+    reports = [
+        cv.get_address,
+        cv.get_band_channel,
+        cv.get_band_group,
+        cv.get_frequency,
+        cv.get_osd_string,
+        cv.get_lock_format,
+        cv.get_mode,
+        cv.get_model_version,
+        cv.get_rssi,
+        cv.get_osd_state,
+        cv.get_video_format,
+    ]
+
+    # Generating a list of reports
+
+    print("Generating a list of reports. Press any key to continue after each report.")
+    input()
+    for report in reports:
+        print(report.__name__)
+        print(report(rx_id))
+        input()
+
+
+    # while True:
+    #     cv.send_report_cstm(rx_id)
 
 
     print("Done")
