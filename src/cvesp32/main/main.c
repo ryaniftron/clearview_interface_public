6a75012dbcaa34338685cbfcb9df88f56014aa12
ryaniftron <ryan.iftron@gmail.com>
2020-03-22 03:52:20 -0600

    Update station code with latest IDF example
    The latest IDF has much better station code with a better event handler.
    Now, using the newer event handler (esp_event). This new code
    has auto retries to the AP.
    
    Also started investigating transferring from AP to STA by
    turning off wifi entirely (using kill_wifi), then
    restarting wifi for the STA. This is probably the easiest method.

--- src/cvesp32/main/main.c
+++ src/cvesp32/main/main.c
@@ -14,7 +14,7 @@
 #include "esp_log.h"
 #include "tcpip_adapter.h"
 static EventGroupHandle_t wifi_event_group;
-const int CONNECTED_BIT = BIT0;
+static const int CONNECTED_BIT = BIT0;
 
 
 #include "lwip/err.h"
@@ -24,14 +24,15 @@ const int CONNECTED_BIT = BIT0;
 
 //Components
 //#include "cv_mqtt.h"
+#include "test.c"
 
 //*****************
 //Network credentials for station mode. 
 //*****************
 
 //If uncommended, these override Kconfig.projbuild
-#define my_network_ssid           "Rachel"
-#define my_network_pass           "woodwood"
+//#define my_network_ssid           "my_ssid_override"
+//#define my_network_pass           "my_pwd_override"
 
 // the run "idf.py menuconfig" and  navigate to example configuration to change your defaults
 #ifndef my_network_ssid
@@ -41,7 +42,7 @@ const int CONNECTED_BIT = BIT0;
 #endif
 
 #ifndef my_network_ssid
-    #define AP_TARGET_PASS            CONFIG_NETWORK_PASS
+    #define AP_TARGET_PASS            CONFIG_NETWORK_PASSWORD
 #else
     #define AP_TARGET_PASS            my_network_pass
 #endif
@@ -52,6 +53,22 @@ const int CONNECTED_BIT = BIT0;
 #define SOFTAP_MAX_STA_CONN       CONFIG_ESP_MAX_STA_CONN
 
 static const char *TAG = "cv-esp32";
+
+bool switch_to_sta = false;
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+static int s_retry_num = 0; //current number of retries ESP32 has tried to connect to AP
+#define ESP_MAXIMUM_RETRY 3
+
+
+
 
 #define HDR_200 "HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n"
 #define HDR_201 "HTTP/1.1 201 Created\r\nContent-type: text/html\r\n\r\n"
@@ -111,28 +128,12 @@ static const char *TAG = "cv-esp32";
     " Error: config_esp32 Parse Fail <br> \
     "
 
-// uint64_t getEfuseMac(void)
-// {
-//     uint64_t _chipmacid = 0LL;
-//     esp_efuse_mac_get_default((uint8_t*) (&_chipmacid));
-//     return _chipmacid;
-// }
-    
-static void get_chip_id(char* ssid, const int UNIQUE_ID_LENGTH){
-    uint64_t chipid = 0LL;
-    esp_efuse_mac_get_default((uint8_t*) (&chipid));
-    uint16_t chip = (uint16_t)(chipid >> 32);
-    //esp_read_mac(chipid);
-    snprintf(ssid, UNIQUE_ID_LENGTH, "CV-%04X%08X", chip, (uint32_t)chipid);
-    printf("SSID created from chip id: %s\n", ssid);
-    return;
-}
 
 static void set_credential(char* credentialName, char* val){
     printf("##Setting %s = %s##", credentialName, val);
+
     // TODO store these values inside ESP32 then join access point
 }
-
 
 // parse the request payload for credentials for the wifi device
 // Successful parse returns true
@@ -185,8 +186,147 @@ static bool parse_net_credentials(char* payload){
             free(arg1);
         }
     }
+    switch_to_sta = true;
     return true;
 }
+
+static void
+http_server_netconn_serve(struct netconn *conn)
+{
+	struct netbuf *inbuf;
+	char *buf, *payload, *start, *stop;
+	u16_t buflen;
+	err_t err;
+
+  /* Read the data from the port, blocking if nothing yet there.
+   We assume the request (the part we care about) is in one netbuf */
+	err = netconn_recv(conn, &inbuf);
+
+	if (err == ERR_OK) {
+        // TODO capture the possible error code form netbuf_data
+		netbuf_data(inbuf, (void**)&buf, &buflen);
+		// find the first and seconds space (those delimt the url)
+		start = strstr(buf, " ") + 1;
+		stop = strstr(start + 1, " ");	
+		// allocate memory for the payload
+		payload = (char *) malloc(stop - start + 1);
+		memcpy(payload, start, stop - start);
+		payload[stop - start] = '\0';
+
+		/* For now  only GET results in a valid respons */
+		if (strncmp(buf, "GET /", 5) == 0){
+            netconn_write(conn, HDR_200, sizeof(HDR_200)-1, NETCONN_NOCOPY);
+			//printf("GET = '%s' \n", payload);
+            if (strcmp(payload, "/") == 0 || strcmp(payload, "/?") == 0) { //give the root website
+                netconn_write(conn, root_text, sizeof(root_text)-1, NETCONN_NOCOPY);
+            }else if (strcmp(payload, "/favicon.ico") == 0) {
+                //do nothing
+            }else if (strcmp(payload, "/scan_wifi?") == 0) {
+                netconn_write(conn, wifi_scan_text, sizeof(wifi_scan_text)-1, NETCONN_NOCOPY);
+            }else if (strncmp(payload, "/config_esp32", strlen("/config_esp32")) == 0){
+                printf("Got a config filestring\n");
+                //GET='GET /config_esp32?ssid=A&password=B&device_name=C '
+                if (parse_net_credentials(payload)){
+                    netconn_write(conn, wifi_attempt_connect, sizeof(wifi_attempt_connect)-1, NETCONN_NOCOPY);
+                    switch_to_sta = true;
+                }else {
+                    netconn_write(conn, config_esp32_parse_fail, sizeof(config_esp32_parse_fail)-1, NETCONN_NOCOPY);
+                }
+            }else{  
+                //char* errmsg = sprintf("Uncaptured GET='%s' \n", *payload);
+                //printf("Uncaptured GET='%s' \n", &payload);
+                //netconn_write(conn, errmsg, sizeof(errmsg)-1, NETCONN_NOCOPY);              
+            }
+
+            
+            /* send "hello world to client" */
+            
+			
+		}else if (strncmp(buf, "POST /", 6) == 0){
+			/* send '501 Not implementd' reply  */
+			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
+		}else if (strncmp(buf, "PUT /", 5) == 0){
+			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
+		}else if (strncmp(buf, "PATCH /", 7) == 0){
+			/* send '501 Not implementd' reply  */
+			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
+		}else if (strncmp(buf, "DELETE /", 8) == 0){
+			/* send '501 Not implementd' reply  */
+			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
+		}else{
+			/* 	Any unrecognized verb will automatically 
+				result in '501 Not implementd' reply */
+			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
+		}
+		free(payload);
+	}
+  	/* Close the connection (server closes in HTTP) and clean up after ourself */
+	netconn_close(conn);
+	netbuf_delete(inbuf);
+}
+
+
+static void http_server(void *pvParameters)
+{
+    
+	uint32_t port;
+	if (pvParameters == NULL){
+		port = 80;
+	}else{
+		port = (uint32_t) pvParameters;
+	}
+	// printf("\n Creating socket at\n %d \n", (uint32_t) pvParameters);
+	struct netconn *conn, *newconn;
+	err_t err;
+	conn = netconn_new(NETCONN_TCP);
+	netconn_bind(conn, NULL,  port);
+	netconn_listen(conn);
+	do {
+		err = netconn_accept(conn, &newconn);
+		if (err == ERR_OK) {
+			http_server_netconn_serve(newconn);
+			netconn_delete(newconn);
+		}
+	} while(err == ERR_OK);
+	netconn_close(conn);
+	netconn_delete(conn);
+    
+}
+
+static void get_chip_id(char* ssid, const int UNIQUE_ID_LENGTH){
+    uint64_t chipid = 0LL;
+    esp_efuse_mac_get_default((uint8_t*) (&chipid));
+    uint16_t chip = (uint16_t)(chipid >> 32);
+    //esp_read_mac(chipid);
+    snprintf(ssid, UNIQUE_ID_LENGTH, "CV-%04X%08X", chip, (uint32_t)chipid);
+    printf("SSID created from chip id: %s\n", ssid);
+    return;
+}
+
+
+static void ap_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:%s",
+                 ip4addr_ntoa(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
 
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
@@ -228,7 +368,7 @@ static void initialize_softAP_wifi(char* PARAM_ESP_WIFI_SSID, uint8_t PARAM_SSID
 {
 // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/wifi.html
     printf("Starting ESP32 softAP mode.\n");
-    tcpip_adapter_init();
+    
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); //VSCODE shows this undefined but it works
@@ -254,7 +394,7 @@ static void initialize_softAP_wifi(char* PARAM_ESP_WIFI_SSID, uint8_t PARAM_SSID
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());    
+    ESP_ERROR_CHECK(esp_wifi_start());
     
     if (strlen(SOFTAP_PASS) == 0){
         ESP_LOGI(TAG, "softAP started. SSID:'%s'",
@@ -272,141 +412,105 @@ static void initialize_softAP_wifi(char* PARAM_ESP_WIFI_SSID, uint8_t PARAM_SSID
 
 static void initialise_sta_wifi(void)
 {
+
+        s_wifi_event_group = xEventGroupCreate();
+
+    tcpip_adapter_init();
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &ap_event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ap_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = AP_TARGET_SSID,
+            .password = AP_TARGET_PASS
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished. Connecting to SSID:%s password:%s",
+             AP_TARGET_SSID, AP_TARGET_PASS);
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 AP_TARGET_SSID, AP_TARGET_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 AP_TARGET_SSID, AP_TARGET_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &ap_event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &ap_event_handler));
+    vEventGroupDelete(s_wifi_event_group);
 	
-	tcpip_adapter_init();
-	wifi_event_group = xEventGroupCreate();
-	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-
-#ifdef DEVICE_IP
-	tcpip_adapter_dhcpc_stop(TCPIP_ADAPTER_IF_STA);
-	tcpip_adapter_ip_info_t ipInfo;
-	inet_pton(AF_INET, DEVICE_IP, &ipInfo.ip);
-	inet_pton(AF_INET, DEVICE_GW, &ipInfo.gw);
-	inet_pton(AF_INET, DEVICE_NETMASK, &ipInfo.netmask);
-	tcpip_adapter_set_ip_info(TCPIP_ADAPTER_IF_STA, &ipInfo);
-#endif
-
-	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-
-    //TODO: Set up both station and AP mode:
-    //  https://www.esp32.com/viewtopic.php?t=10619
-
-
-	wifi_config_t sta_config = {
-		.sta = {
-			.ssid = AP_TARGET_SSID,
-			.password = AP_TARGET_PASS,
-			.bssid_set = false
-		}
-	};
-
-  ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-  ESP_ERROR_CHECK( esp_wifi_start() );
 }
 
-static void
-http_server_netconn_serve(struct netconn *conn)
+//disconnect and kill wifi driver no matter which mode it is in
+void kill_wifi(void){
+    wifi_mode_t mode;
+    ESP_ERROR_CHECK(esp_wifi_get_mode(&mode));
+
+    if (mode == WIFI_MODE_STA){ ///TODO this condition needs testing on all three modes
+        //Answer the question: When do you need to run esp_wifi_disconnect. 
+        //Confirmed: Don't run in WIFI_MODE_AP because there is nothing to disconnect from and it breaks.
+        ESP_ERROR_CHECK(esp_wifi_disconnect());
+    }
+    ESP_ERROR_CHECK(esp_wifi_stop());
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+}
+
+static void demo_sequential_wifi(char* PARAM_ESP_WIFI_SSID, uint8_t PARAM_SSID_LEN)
 {
-	struct netbuf *inbuf;
-	char *buf, *payload, *start, *stop;
-	u16_t buflen;
-	err_t err;
+    printf("Starting ESP32 both mode.\n"); 
+    initialize_softAP_wifi(PARAM_ESP_WIFI_SSID,PARAM_SSID_LEN);
+    printf("Starting http server\n");
+    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
+    printf("Server started\n");
+    while (true){
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        printf("Waiting for  switch_to_sta\n");
+        if (switch_to_sta == true)
+        {   
+            printf("Switching now\n");
+            switch_to_sta = false;
+           
+            break;
+        }
+    }
+    printf("Onward! esp_wifi_stop has been run. Time to start station.... (TODO) \n ");
+    //https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/wifi.html#wi-fi-deinit-phase
 
-  /* Read the data from the port, blocking if nothing yet there.
-   We assume the request (the part we care about) is in one netbuf */
-	err = netconn_recv(conn, &inbuf);
+    kill_wifi();
+    
+    printf("Ready to try station wifi\n"); //TODO is this printed or stuck in kill_wifi
+    //initialise_sta_wifi();
 
-	if (err == ERR_OK) {
-        // TODO capture the possible error code form netbuf_data
-		netbuf_data(inbuf, (void**)&buf, &buflen);
-		// find the first and seconds space (those delimt the url)
-		start = strstr(buf, " ") + 1;
-		stop = strstr(start + 1, " ");	
-		// allocate memory for the payload
-		payload = (char *) malloc(stop - start + 1);
-		memcpy(payload, start, stop - start);
-		payload[stop - start] = '\0';
 
-		/* For now  only GET results in a valid respons */
-		if (strncmp(buf, "GET /", 5) == 0){
-            netconn_write(conn, HDR_200, sizeof(HDR_200)-1, NETCONN_NOCOPY);
-			//printf("GET = '%s' \n", payload);
-            if (strcmp(payload, "/") == 0 || strcmp(payload, "/?") == 0) { //give the root website
-                netconn_write(conn, root_text, sizeof(root_text)-1, NETCONN_NOCOPY);
-            }else if (strcmp(payload, "/favicon.ico") == 0) {
-                //do nothing
-            }else if (strcmp(payload, "/scan_wifi?") == 0) {
-                netconn_write(conn, wifi_scan_text, sizeof(wifi_scan_text)-1, NETCONN_NOCOPY);
-            }else if (strncmp(payload, "/config_esp32", strlen("/config_esp32")) == 0){
-                printf("Got a config filestring\n");
-                //GET='GET /config_esp32?ssid=A&password=B&device_name=C '
-                if (parse_net_credentials(payload)){
-                    netconn_write(conn, wifi_attempt_connect, sizeof(wifi_attempt_connect)-1, NETCONN_NOCOPY);
-                }else {
-                    netconn_write(conn, config_esp32_parse_fail, sizeof(config_esp32_parse_fail)-1, NETCONN_NOCOPY);
-                }
-            }else{  
-                //char* errmsg = sprintf("Uncaptured GET='%s' \n", *payload);
-                //printf("Uncaptured GET='%s' \n", &payload);
-                //netconn_write(conn, errmsg, sizeof(errmsg)-1, NETCONN_NOCOPY);              
-            }
 
-            
-            /* send "hello world to client" */
-            
-			
-		}else if (strncmp(buf, "POST /", 6) == 0){
-			/* send '501 Not implementd' reply  */
-			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
-		}else if (strncmp(buf, "PUT /", 5) == 0){
-			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
-		}else if (strncmp(buf, "PATCH /", 7) == 0){
-			/* send '501 Not implementd' reply  */
-			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
-		}else if (strncmp(buf, "DELETE /", 8) == 0){
-			/* send '501 Not implementd' reply  */
-			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
-		}else{
-			/* 	Any unrecognized verb will automatically 
-				result in '501 Not implementd' reply */
-			netconn_write(conn, HDR_501, sizeof(HDR_501)-1, NETCONN_NOCOPY);
-		}
-		free(payload);
-	}
-  	/* Close the connection (server closes in HTTP) and clean up after ourself */
-	netconn_close(conn);
-	netbuf_delete(inbuf);
 }
 
-static void http_server(void *pvParameters)
-{
-    
-	uint32_t port;
-	if (pvParameters == NULL){
-		port = 80;
-	}else{
-		port = (uint32_t) pvParameters;
-	}
-	// printf("\n Creating socket at\n %d \n", (uint32_t) pvParameters);
-	struct netconn *conn, *newconn;
-	err_t err;
-	conn = netconn_new(NETCONN_TCP);
-	netconn_bind(conn, NULL,  port);
-	netconn_listen(conn);
-	do {
-		err = netconn_accept(conn, &newconn);
-		if (err == ERR_OK) {
-			http_server_netconn_serve(newconn);
-			netconn_delete(newconn);
-		}
-	} while(err == ERR_OK);
-	netconn_close(conn);
-	netconn_delete(conn);
-    
-}
+
+
+
 
 
 void app_main(void)
@@ -418,7 +522,8 @@ void app_main(void)
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
-    
+
+    tcpip_adapter_init();
 
     const int UNIQUE_ID_LENGTH = 16;
     char chipid[UNIQUE_ID_LENGTH];
@@ -427,12 +532,14 @@ void app_main(void)
 
     //Start Wifi
 	//initialise_sta_wifi();
-    initialize_softAP_wifi(chipid, UNIQUE_ID_LENGTH);
+    //initialize_softAP_wifi(chipid, UNIQUE_ID_LENGTH);
+    demo_sequential_wifi(chipid, UNIQUE_ID_LENGTH);
 
-    printf("Starting http server\n");
-    xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
+   
 
     //cv_mqtt_init();
+    //printf("Starting http server\n");
+    //xTaskCreate(&http_server, "http_server", 2048, NULL, 5, NULL);
 
     
     vTaskDelay(1000 / portTICK_PERIOD_MS);
