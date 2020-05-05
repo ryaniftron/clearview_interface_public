766c123291be6b91570a8be029b385a92810af0c
ryaniftron <ryan.iftron@gmail.com>
2020-05-04 22:42:39 -0600

    Add OTA stub functions to support firmware updates wirelessly:

---
+++ src/cvesp32/main/cv_ota.c
@@ -0,0 +1,5 @@
+// This file handles the OTA (wireless) updating of the ESP32. 
+
+// OTA functionality has been adapted from the following sources
+// https://github.com/espressif/esp-idf/tree/1aebfdf/examples/system/ota
+// https://github.com/versamodule/ESP32-OTA-Commandline
