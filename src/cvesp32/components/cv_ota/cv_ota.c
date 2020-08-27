4e4d738147cbf59b173734b96a7cb0b719cb55fe
ryaniftron <ryan.iftron@gmail.com>
2020-08-26 23:56:31 -0600

    Change from index.html to ota.html filename

--- src/cvesp32/components/cv_ota/cv_ota.c
+++ src/cvesp32/components/cv_ota/cv_ota.c
@@ -14,8 +14,8 @@
 #include "freertos/event_groups.h"
 
 // Embedded Files. To add or remove make changes is component.mk file as well. 
-extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[]   asm("_binary_index_html_end");
+extern const uint8_t index_html_start[] asm("_binary_ota_html_start");
+extern const uint8_t index_html_end[]   asm("_binary_ota_html_end");
 extern const uint8_t favicon_ico_start[] asm("_binary_favicon_ico_start");
 extern const uint8_t favicon_ico_end[]   asm("_binary_favicon_ico_end");
 extern const uint8_t jquery_3_4_1_min_js_start[] asm("_binary_jquery_3_4_1_min_js_start");
