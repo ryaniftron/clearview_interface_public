a7c816e49fd44d281e8b2a386dee48b548177e66
ryaniftron <ryan.iftron@gmail.com>
2020-09-29 01:46:21 -0600

    Improve includes

--- src/cvesp32/components/cv_server/include/cv_server.h
+++ src/cvesp32/components/cv_server/include/cv_server.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "esp_http_server.h"
+#include <cJSON.h>
+#include <cJSON_Utils.h>
+
 extern httpd_handle_t start_cv_webserver(void);
 extern void serve_title(httpd_req_t *req); 
 extern void serve_seat_and_lock(httpd_req_t *req);
@@ -9,3 +12,5 @@ extern void serve_test(httpd_req_t *req);
 extern void serve_html_beg(httpd_req_t *req);
 extern void serve_html_end(httpd_req_t *req);
 extern void serve_menu_bar(httpd_req_t *req);
+// extern bool json_api_handle(char* buf, cJSON* j);
+extern bool json_api_handle(char* buf, cJSON* ret);
